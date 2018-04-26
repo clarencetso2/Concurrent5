0cb55f22f022151b3f26a090baef4235e8777df8
Clarence Tso <clarence.tso.2@gmail.com>
2018-04-26 15:45:32 -0500

    please  me kevin brill

--- paxos/PaxosRunnble.java
+++ paxos/PaxosRunnble.java
@@ -38,19 +38,22 @@ public class PaxosRunnble implements Runnable{
 
         int count1 = 0;
         int count2 = 0;
+        
+        int received_n = -1;
+        Object received_v = null;
 
         //Need to keep track of the state
         while(paxos.states.get(localSeq)!=Decided) {
-            synchronized (this) {
                 paxos.lamportClock = paxos.proposer_n.get(seq);
                 paxos.lamportClock++;
                 paxos.n.put(localSeq, paxos.lamportClock);
-          //      paxos.proposing.put(localSeq, true);
+        //      paxos.proposing.put(localSeq, true);
 
 
 
-            }
-
+            // recieved n , received value, update it
+            
+            ////////////////////////////////Phase 1///////////////////////////////////////////////////////
             for (int i = 0; i < paxos.peers.length; i++) {
             	Response prepResponse;
             	if(i == me){
@@ -59,56 +62,70 @@ public class PaxosRunnble implements Runnable{
             	else{
             		 prepResponse = paxos.Call("Prepare", new Request(localSeq, localVal, paxos.n.get(localSeq),me,paxos.highestDone.get(me)), i);
             	}
-            	if(prepResponse != null && prepResponse.decided){
-            	    paxos.accept_v.put(localSeq, prepResponse.value);
-                }
+            	
+            	if (prepResponse != null && prepResponse.acceptNum >= received_n) {
+                      received_v = prepResponse.value;
+                      received_n = prepResponse.acceptNum;
+                  }
+
                 if (prepResponse != null && prepResponse.ack) {
                     count1++;
-                    if (count1 >= (paxos.peers.length / 2) + 1) {
-                        if (prepResponse.acceptNum >= max_na && prepResponse.acceptNum != -1) {
-                            localVal = prepResponse.value;
-                            max_na = prepResponse.acceptNum;
-                        }
-                        
-                        
-                        for(int j = 0; j < paxos.peers.length; j++) {
-                        	Response accResponse; 
-                        	if(j == me){
-                                accResponse = paxos.Accept(new Request(localSeq, localVal, paxos.n.get(localSeq),me, paxos.highestDone.get(me)));
+                }
+            }
+                    
+                
+            if (count1 >= (paxos.peers.length / 2) + 1) {
+            	if (received_n != -1 && received_v != null) {
+            		localVal = received_v;
+                    max_na = received_n;
+                }
+                                
+                for(int j = 0; j < paxos.peers.length; j++) {
+                	Response accResponse; 
+                   	if(j == me){
+                   		accResponse = paxos.Accept(new Request(localSeq, localVal, paxos.n.get(localSeq),me, paxos.highestDone.get(me)));
                         		
-                        	}
-                        	else{
-                                accResponse = paxos.Call("Accept", new Request(localSeq, localVal, paxos.n.get(localSeq), me,paxos.highestDone.get(me)), j);
-
-                        	}
-                            if (accResponse != null && accResponse.ack) {
-                                count2++;
-                                if (count2 >= (paxos.peers.length / 2) + 1) {
-                                    //printState(localSeq, paxos.states.get(localSeq));
-                                    for(int k = 0; k < paxos.peers.length; k++){
-                                    	Response decideResponse;
-                                    	if(k==me){
-                                    		decideResponse = paxos.Decide(new Request(localSeq, localVal, paxos.n.get(localSeq), me, paxos.highestDone.get(me)));
-                                    	}
-                                    	else{
-                                    		decideResponse = paxos.Call("Decide", new Request(localSeq, localVal, paxos.n.get(localSeq), me,paxos.highestDone.get(me)), k);
-                                    	}
-                                    }
-
-                                    i = j = paxos.peers.length;
-                                }
-                            }
-                        }
                     }
+                    else{
+                        accResponse = paxos.Call("Accept", new Request(localSeq, localVal, paxos.n.get(localSeq), me,paxos.highestDone.get(me)), j);
+
+                    }
+                    if (accResponse != null && accResponse.ack) {
+                        count2++;
+                    }
+                }
+
+                if (count2 >= (paxos.peers.length / 2) + 1) {
+                    //printState(localSeq, paxos.states.get(localSeq));
+                    for(int k = 0; k < paxos.peers.length; k++){
+                    	Response decideResponse;
+                    	if(k==me){
+                    		decideResponse = paxos.Decide(new Request(localSeq, localVal, paxos.n.get(localSeq), me, paxos.highestDone.get(me)));
+                    	}
+                    	else{
+                    		decideResponse = paxos.Call("Decide", new Request(localSeq, localVal, paxos.n.get(localSeq), me,paxos.highestDone.get(me)), k);
+                    	}
+                    }
+
                 }
             }
         }
-        //call Done(localSeq)
-
-
-      // retStatus status = paxos.Status(localSeq);
-
     }
+
+
+                        
+                        
+                        
+                        
+                        
+                            
+        
+       //call Done(localSeq)
+
+
+      //retStatus status = paxos.Status(localSeq);
+
+    
 
     public void printState(int s, State state){
         if(state == Decided){
--- paxos/PaxosTest.java
+++ paxos/PaxosTest.java
@@ -77,159 +77,159 @@ public class PaxosTest {
         return pxa;
     }
 
-//    @Test
-//    public void TestBasic(){
-//
-//        final int npaxos = 5;
-//        Paxos[] pxa = initPaxos(npaxos);
-//
-//        System.out.println("Test: Single proposer ...");
-//        pxa[0].Start(0, "hello");
-//        waitn(pxa, 0, npaxos);
-//        System.out.println("... Passed");
-//
-//
-//        System.out.println("Test: Many proposers, same value ...");
-//        for(int i = 0; i < npaxos; i++){
-//            pxa[i].Start(1, 77);
-//        }
-//        waitn(pxa, 1, npaxos);
-//        System.out.println("... Passed");
-//
-//        System.out.println("Test: Many proposers, different values ...");
-//        pxa[0].Start(2, 100);
-//        pxa[1].Start(2, 101);
-//        pxa[2].Start(2, 102);
-//        waitn(pxa, 2, npaxos);
-//        System.out.println("... Passed");
-//
-//        System.out.println("Test: Out-of-order instances ...");
-//        pxa[0].Start(7, 700);
-//        try {
-//            Thread.sleep(10);
-//        } catch (Exception e){
-//            e.printStackTrace();
-//        }
-//        pxa[0].Start(6, 600);
-//        pxa[1].Start(5, 500);
-//        waitn(pxa, 7, npaxos);
-//        pxa[0].Start(4, 400);
-//        pxa[1].Start(3, 300);
-//        waitn(pxa, 6, npaxos);
-//        waitn(pxa, 5, npaxos);
-//        waitn(pxa, 4, npaxos);
-//        waitn(pxa, 3, npaxos);
-//        System.out.println("... Passed");
-//        cleanup(pxa);
-//
-//    }
-
-//   @Test
-//    public void TestDeaf(){
-//
-//        final int npaxos = 5;
-//        Paxos[] pxa = initPaxos(npaxos);
-//
-//        System.out.println("Test: Deaf proposer ...");
-//        pxa[0].Start(0, "hello");
-//        waitn(pxa, 0, npaxos);
-//
-//        pxa[1].ports[0]= 1;
-//        pxa[1].ports[npaxos-1]= 1;
-//        pxa[1].Start(1, "goodbye");
-//        waitmajority(pxa, 1);
-//        try {
-//            Thread.sleep(1000);
-//        } catch (Exception e){
-//            e.printStackTrace();
-//        }
-//        int nd = ndecided(pxa, 1);
-//        assertFalse("a deaf peer heard about a decision " + nd, nd != npaxos-2);
-//
-//        System.out.println("Beginning failed test.");
-//        pxa[0].Start(1, "xxx");
-//        waitn(pxa, 1, npaxos-1);
-//        try {
-//            Thread.sleep(1000);
-//        } catch (Exception e){
-//            e.printStackTrace();
-//        }
-//        nd = ndecided(pxa, 1);
-//        assertFalse("a deaf peer heard about a decision " + nd, nd != npaxos-1);
-//
-//        pxa[npaxos-1].Start(1, "yyy");
-//        waitn(pxa, 1, npaxos);
-//        System.out.println("... Passed");
-//        cleanup(pxa);
-//
-//    }
-
     @Test
-    public void TestForget(){
+    public void TestBasic(){
 
-        final int npaxos = 6;
+        final int npaxos = 5;
         Paxos[] pxa = initPaxos(npaxos);
 
-        System.out.println("Test: Forgetting ...");
-
-        for(int i = 0; i < npaxos; i++){
-            int m = pxa[i].Min();
-            assertFalse("Wrong initial Min() " + m, m > 0);
-        }
-
-        pxa[0].Start(0,"00");
-        pxa[1].Start(1,"11");
-        pxa[2].Start(2,"22");
-        pxa[0].Start(6,"66");
-        pxa[1].Start(7,"77");
-
+        System.out.println("Test: Single proposer ...");
+        pxa[0].Start(0, "hello");
         waitn(pxa, 0, npaxos);
-        for(int i = 0; i < npaxos; i++){
-            int m = pxa[i].Min();
-            assertFalse("Wrong Min() " + m + "; expected 0", m != 0);
-        }
+        System.out.println("... Passed");
 
+
+        System.out.println("Test: Many proposers, same value ...");
+        for(int i = 0; i < npaxos; i++){
+            pxa[i].Start(1, 77);
+        }
         waitn(pxa, 1, npaxos);
-        for(int i = 0; i < npaxos; i++){
-            int m = pxa[i].Min();
-            assertFalse("Wrong Min() " + m + "; expected 0", m != 0);
-        }
+        System.out.println("... Passed");
 
-        for(int i = 0; i < npaxos; i++){
-            pxa[i].Done(0);
-        }
+        System.out.println("Test: Many proposers, different values ...");
+        pxa[0].Start(2, 100);
+        pxa[1].Start(2, 101);
+        pxa[2].Start(2, 102);
+        waitn(pxa, 2, npaxos);
+        System.out.println("... Passed");
 
-        for(int i = 1; i < npaxos; i++){
-            pxa[i].Done(1);
+        System.out.println("Test: Out-of-order instances ...");
+        pxa[0].Start(7, 700);
+        try {
+            Thread.sleep(10);
+        } catch (Exception e){
+            e.printStackTrace();
         }
-
-        for(int i = 0; i < npaxos; i++){
-            pxa[i].Start(8+i, "xx");
-        }
-
-        boolean ok = false;
-        for(int iters = 0; iters < 12; iters++){
-            ok = true;
-            for(int i = 0; i < npaxos; i++){
-                int s = pxa[i].Min();
-                if(s != 1){
-                    ok = false;
-                }
-            }
-            if(ok) break;
-            try {
-                Thread.sleep(1000);
-            } catch (Exception e){
-                e.printStackTrace();
-            }
-
-        }
-        assertFalse("Min() did not advance after Done()", ok != true);
+        pxa[0].Start(6, 600);
+        pxa[1].Start(5, 500);
+        waitn(pxa, 7, npaxos);
+        pxa[0].Start(4, 400);
+        pxa[1].Start(3, 300);
+        waitn(pxa, 6, npaxos);
+        waitn(pxa, 5, npaxos);
+        waitn(pxa, 4, npaxos);
+        waitn(pxa, 3, npaxos);
         System.out.println("... Passed");
         cleanup(pxa);
 
+    }
+
+   @Test
+    public void TestDeaf(){
+
+        final int npaxos = 5;
+        Paxos[] pxa = initPaxos(npaxos);
+
+        System.out.println("Test: Deaf proposer ...");
+        pxa[0].Start(0, "hello");
+        waitn(pxa, 0, npaxos);
+
+        pxa[1].ports[0]= 1;
+        pxa[1].ports[npaxos-1]= 1;
+        pxa[1].Start(1, "goodbye");
+        waitmajority(pxa, 1);
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        int nd = ndecided(pxa, 1);
+        assertFalse("a deaf peer heard about a decision " + nd, nd != npaxos-2);
+
+        System.out.println("Beginning failed test.");
+        pxa[0].Start(1, "xxx");
+        waitn(pxa, 1, npaxos-1);
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        nd = ndecided(pxa, 1);
+        assertFalse("a deaf peer heard about a decision " + nd, nd != npaxos-1);
+
+        pxa[npaxos-1].Start(1, "yyy");
+        waitn(pxa, 1, npaxos);
+        System.out.println("... Passed");
+        cleanup(pxa);
 
     }
+
+//    @Test
+//    public void TestForget(){
+//
+//        final int npaxos = 6;
+//        Paxos[] pxa = initPaxos(npaxos);
+//
+//        System.out.println("Test: Forgetting ...");
+//
+//        for(int i = 0; i < npaxos; i++){
+//            int m = pxa[i].Min();
+//            assertFalse("Wrong initial Min() " + m, m > 0);
+//        }
+//
+//        pxa[0].Start(0,"00");
+//        pxa[1].Start(1,"11");
+//        pxa[2].Start(2,"22");
+//        pxa[0].Start(6,"66");
+//        pxa[1].Start(7,"77");
+//
+//        waitn(pxa, 0, npaxos);
+//        for(int i = 0; i < npaxos; i++){
+//            int m = pxa[i].Min();
+//            assertFalse("Wrong Min() " + m + "; expected 0", m != 0);
+//        }
+//
+//        waitn(pxa, 1, npaxos);
+//        for(int i = 0; i < npaxos; i++){
+//            int m = pxa[i].Min();
+//            assertFalse("Wrong Min() " + m + "; expected 0", m != 0);
+//        }
+//
+//        for(int i = 0; i < npaxos; i++){
+//            pxa[i].Done(0);
+//        }
+//
+//        for(int i = 1; i < npaxos; i++){
+//            pxa[i].Done(1);
+//        }
+//
+//        for(int i = 0; i < npaxos; i++){
+//            pxa[i].Start(8+i, "xx");
+//        }
+//
+//        boolean ok = false;
+//        for(int iters = 0; iters < 12; iters++){
+//            ok = true;
+//            for(int i = 0; i < npaxos; i++){
+//                int s = pxa[i].Min();
+//                if(s != 1){
+//                    ok = false;
+//                }
+//            }
+//            if(ok) break;
+//            try {
+//                Thread.sleep(1000);
+//            } catch (Exception e){
+//                e.printStackTrace();
+//            }
+//
+//        }
+//        assertFalse("Min() did not advance after Done()", ok != true);
+//        System.out.println("... Passed");
+//        cleanup(pxa);
+//
+//
+//    }
 
 
 }
